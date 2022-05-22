a5d5d149e4142b0fe5f54e39e4d760255378c036
G-Srija99 <84491504+G-Srija99@users.noreply.github.com>
2022-05-22 22:18:52 +0530

    n=int(input()) a=list(map(int,input().strip().split())) even=0 odd=0 s=0 for i in range(n):     if a[i]%2==0:         even=even+a[i]     else:         odd=odd+a[i] s=odd+even print(s)

---
+++ Sum_of_Array.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().strip().split()))
+even=0
+odd=0
+s=0
+for i in range(n):
+    if a[i]%2==0:
+        even=even+a[i]
+    else:
+        odd=odd+a[i]
+s=odd+even
+print(s)
+    
